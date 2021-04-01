5274148062b02277c192a421d6df3f4ae4060183
njy <394608294@qq.com>
2021-04-01 22:21:58 +0800

    增加部署配置

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM node:alpine as builder
+
+COPY public ./app/public
+COPY src ./app/src
+COPY env ./app/env
+COPY craco.config.js ./app
+COPY package.json ./app
+COPY package-lock.json ./app
+COPY tsconfig.json ./app
+COPY type.d.ts ./app
+COPY yarn.lock ./app
+
+WORKDIR '/app'
+
+RUN npm config set registry https://registry.npm.taobao.org
+RUN yarn install
+RUN yarn build
+
+
+FROM nginx
+EXPOSE 80
+EXPOSE 443
+RUN sed -i "s@http://deb.debian.org@http://mirrors.163.com@g" /etc/apt/sources.list \
+    && apt-get update
+RUN yes | apt-get install wget
+
+COPY --from=builder /app/build /var/www
+RUN rm /etc/nginx/conf.d/default.conf
+RUN rm /etc/nginx/nginx.conf
+WORKDIR '/etc/nginx'
+
+RUN wget http://192.168.0.130/file_exchange/prod/nginx.conf \
+    && wget http://192.168.0.130/file_exchange/prod/ssl/portal.happyworkhardlife.com.key \
+    && wget http://192.168.0.130/file_exchange/prod/ssl/portal.happyworkhardlife.com.pem \
+    && wget http://192.168.0.130/file_exchange/prod/ssl/www.portal.happyworkhardlife.com.key \
+    && wget http://192.168.0.130/file_exchange/prod/ssl/www.portal.happyworkhardlife.com.pem
+WORKDIR '/'
--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,7 @@
 const CracoAntDesignPlugin = require('craco-antd');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
 const pathResolve = pathUrl => path.join(__dirname, pathUrl);
 
@@ -16,6 +19,47 @@ module.exports = {
             '@store': pathResolve('src/store'),
             '@components': pathResolve('src/components')
         },
+        plugins: [
+            //new BundleAnalyzerPlugin(),
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    compress: {
+                        warnings: false,
+                        drop_debugger: true,
+                        drop_console: true,
+                    },
+                },
+                sourceMap: false,
+                parallel: true,
+            }),
+            // 打压缩包
+            new CompressionWebpackPlugin({
+                algorithm: 'gzip',
+                test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
+                threshold: 1024,
+                minRatio: 1
+            })
+        ],
+        //抽离公用模块
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        chunks: 'initial',
+                        minChunks: 2,
+                        maxInitialRequests: 5,
+                        minSize: 0
+                    },
+                    vendor: {
+                        test: /node_modules/,
+                        chunks: 'initial',
+                        name: 'vendor',
+                        priority: 10,
+                        enforce: true
+                    }
+                }
+            }
+        }
     },
     devServer: {
         hot: true,
